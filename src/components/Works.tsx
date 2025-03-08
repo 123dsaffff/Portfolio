fce7e5c7fb99aa6896b6caf563b6c417796e5256
123dsaffff <lyutovadim0@gmail.com>
2025-03-08 19:06:29 +0300

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -86,7 +86,7 @@ export function Works() {
         {/* Filter buttons */}
         <div className="flex flex-wrap justify-center gap-3 mb-12">
           {filters.map((filter) => (
-            <img>src/components/vk.png</img>
+            <img src="src/components/vk.png" alt=""></img>
             <button
               key={filter.id}
               onClick={() => setActiveFilter(filter.id)}
