1c83415b2bf3ac29705688fcb09d4a0c092b15b1
123dsaffff <lyutovadim0@gmail.com>
2025-03-08 16:30:58 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -84,7 +84,7 @@ export function Header() {
             className="btn btn-primary px-6 py-2 mt-4"
             onClick={() => setMobileMenuOpen(false)}
           >
-            Hire Me
+            Связаться
           </a>
         </div>
       </div>
