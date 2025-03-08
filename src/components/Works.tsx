c3c15408b6d7256bf246338f8caf4134b63af8d4
123dsaffff <lyutovadim0@gmail.com>
2025-03-08 18:15:11 +0300

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -62,7 +62,7 @@ export function Works() {
       id: 4,
       title: "5HTML VK Games",
       description: "Раздел с играми от ВК",
-      image: "https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&w=600&h=400&q=80",
+      image: "Главная.png",
       category: "web",
       link: "#"
     }
