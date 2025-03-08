5751b75cc8bfd5ea6b296a896db28efd0a9e62de
123dsaffff <lyutovadim0@gmail.com>
2025-03-08 19:10:59 +0300

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -46,7 +46,7 @@ export function Works() {
       id: 2,
       title: "Приложение для банка ВТБ",
       description: "Мобильное приложение для обмена криптовалют напрямую между банком и пользователем",
-      image: "https://postimg.cc/7fftKjcc",
+      image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?auto=format&fit=crop&w=600&h=400&q=80",
       category: "app",
       link: "https://www.behance.net/"
     },
